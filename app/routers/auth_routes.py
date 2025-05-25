3c599ba8f61c90b178d49626f23b4ffb31410d76
umeshkedimi <umesh836@gmail.com>
2025-05-25 12:13:38 +0530

    Added login functionality

--- app/routers/auth_routes.py
+++ app/routers/auth_routes.py
@@ -3,6 +3,12 @@ from sqlalchemy.orm import Session
 from app.db.schemas import UserCreate, UserOut
 from app.db.crud.user import create_user, get_user_by_username
 from app.db.db_setup import get_db
+from app.utils.auth import verify_password, create_access_token
+from pydantic import BaseModel
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
 
 router = APIRouter()
 
@@ -13,3 +19,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Username already exists")
     
     return create_user(db, user)
+
+
+@router.post("/login")
+def login(req: LoginRequest, db: Session = Depends(get_db)):
+    user = get_user_by_username(db, req.username)
+    if not user or not verify_password(req.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({"sub": user.username})
+    return {"access_token": token, "token_type": "bearer"}
--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,5 +1,8 @@
 import os
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, status
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 API_KEY = os.getenv("API_KEY_HEADER")
 
@@ -7,3 +10,32 @@ def verify_api_key(request: Request):
     header_key = request.headers.get("X-API-KEY")
     if not header_key or header_key != API_KEY:
         raise HTTPException(status_code=403, detail="Unauthorized access")
+    
+
+
+
+SECRET_KEY = os.getenv("JWT_SECRET", "supersecret")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+

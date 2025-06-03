3709024390f84ae969c9e2f32dae5a03a19dd060
umeshkedimi <umesh836@gmail.com>
2025-06-03 10:11:36 +0530

    Updated broker-account model with telegram_bot_token, target, stoploss

--- app/db/models/broker_account.py
+++ app/db/models/broker_account.py
@@ -1,6 +1,6 @@
 # app/db/models/broker_account.py
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime, Float
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.db.db_setup import Base
@@ -11,16 +11,21 @@ class BrokerAccount(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
-    broker_name = Column(String, nullable=False, default="dhan")
+    broker_name = Column(String, nullable=False)
     client_id = Column(String, nullable=False)
     access_token = Column(String, nullable=False)
 
     telegram_chat_id = Column(String, nullable=True)
+    telegram_bot_token = Column(String, nullable=True)
+
     lot_size = Column(Integer, nullable=False, default=15)
     index = Column(String, nullable=False)  # "nifty" or "banknifty"
-    direction = Column(String, nullable=False)  # "buy" or "sell"
-    is_active = Column(Boolean, default=True)
+    direction = Column(String, nullable=False) # "buy" or "sell"
+    
+    stop_loss = Column(Float, nullable=True)
+    target = Column(Float, nullable=True)
 
+    is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
     user = relationship("User", back_populates="broker_accounts")
